7640022f70a35c96a2ddd6c429a3ed8057006c06
Dzianis <paskin.denis.00gmail.com>
2022-07-07 19:49:48 +0200

    added the first difficult task

---
+++ lesson19/difficult_1.js
@@ -0,0 +1,19 @@
+const input = document.querySelector('input')
+const debounceText = document.getElementById('debounce')
+
+
+const updateDebounceText = debounce((text) =>{
+  debounceText.textContent = text
+})
+
+input.addEventListener('input', e => {
+  updateDebounceText(e.target.value)
+})
+
+function debounce(cb, delay = 300) {
+  return (...args) => {
+    setTimeout(() => {
+      cb(...args)
+    }, delay)
+  }
+}
