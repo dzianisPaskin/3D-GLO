fadab4482ef1919f674e89e218a92eeb89c7b668
Dzianis <paskin.denis.00gmail.com>
2022-07-14 23:11:04 +0200

    completed an additional task

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,19 +1,41 @@
 const menu = () => {
-  const menuBtn = document.querySelector(".menu");
+  // const menuBtn = document.querySelector(".menu");
+  // const menu = document.querySelector("menu");
+  // const main = document.querySelector('body')
+
+  // const toggleMenu = () => {
+  //   menu.classList.toggle('active-menu')
+  // };
+
+  // menu.addEventListener('click', (e) => {
+  //   if(e.target.classList.contains('close-btn') || e.target.classList.contains('close-link')) {
+  //     toggleMenu()
+  //   }
+  // })
+
+  // menuBtn.addEventListener("click", toggleMenu);
+
   const menu = document.querySelector("menu");
+  const body = document.querySelector("body");
+  let showMenu = 'false';
 
-  const toggleMenu = () => {
-    menu.classList.toggle('active-menu')
-  };
-
-  menu.addEventListener('click', (e) => {
-    if(e.target.classList.contains('close-btn') || e.target.classList.contains('close-link')) {
-      toggleMenu()
+  body.addEventListener("click", (e) => {
+    
+    if (e.target.closest(".menu")) {
+      menu.classList.toggle("active-menu");
+      showMenu = 'true';
+    } else if (
+      e.target.classList.contains("close-btn") ||
+      e.target.classList.contains("close-link")
+    ) {
+      menu.classList.toggle("active-menu");
+      showMenu = 'false';
+    } else if (!e.target.classList.contains("active-menu") && showMenu == 'true') {
+      console.log(e.target)
+      menu.classList.toggle("active-menu");
+      showMenu = 'false';
     }
-  })
-
-  menuBtn.addEventListener("click", toggleMenu);
-  
+  });
 };
 
 export default menu;
