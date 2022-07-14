20d530de24b5ef340d00b44fa12b46ebaf7b3172
Dzianis <paskin.denis.00gmail.com>
2022-07-14 09:55:13 +0200

    added lesson 22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,17 +1,18 @@
 const menu = () => {
   const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
-  const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a");
 
-  const handleMenu = () => {
+  const toggleMenu = () => {
     menu.classList.toggle('active-menu')
   };
 
-  menuBtn.addEventListener("click", handleMenu);
-  closeBtn.addEventListener("click", handleMenu);
+  menu.addEventListener('click', (e) => {
+    if(e.target.classList.contains('close-btn') || e.target.classList.contains('close-link')) {
+      toggleMenu()
+    }
+  })
 
-  menuItems.forEach(menu => menu.addEventListener("click", handleMenu))
+  menuBtn.addEventListener("click", toggleMenu);
   
 };
 
