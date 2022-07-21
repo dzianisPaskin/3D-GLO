eca17229033b31e8e46328cb11c1fe2c7e66ba33
Dzianis <paskin.denis.00gmail.com>
2022-07-21 20:20:07 +0200

    added a difficult task of lesson 24

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,4 +1,4 @@
-import {animate} from './helpers'
+import { animate } from "./helpers";
 
 const calc = (price = 100) => {
   const calcBlock = document.querySelector(".calc-block");
@@ -9,43 +9,70 @@ const calc = (price = 100) => {
   const total = document.getElementById("total");
 
   const countCalc = () => {
-    const calcTypeValue = +calcType.options[calcType.selectedIndex].value
-    const calcSquareValue = calcSquare.value
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = calcSquare.value;
 
-    let totalValue = 0
-    let calcCountValue = 1
-    let calcDayValue = 1
+    let totalValue = 0;
+    let calcCountValue = 1;
+    let calcDayValue = 1;
 
-    if(calcCount.value > 1) {
-    calcCountValue += +calcCount.value / 10
+    if (calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
     }
 
-    if(calcDay.value && calcDay.value < 5) {
-      calcDayValue = 2
+    if (calcDay.value && calcDay.value < 5) {
+      calcDayValue = 2;
     } else if (calcDay.value && calcDay.value < 10) {
-      calcDayValue = 1.5
+      calcDayValue = 1.5;
     }
 
-    if(calcType.value && calcSquare.value) {
-
-        totalValue = price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue
+    if (calcType.value && calcSquare.value) {
+      totalValue =
+        price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
     } else {
-      totalValue = 0
+      totalValue = 0;
     }
+
     
-    total.textContent = totalValue
+    
+    if (
+      calcType.value != "" &&
+      calcSquare.value != "" &&
+      calcCount.value != "" &&
+      calcDay.value != ""
+    ) {
+      let timeInterval = 1000 * 2 / totalValue
+      let i = 0;
+      const intervalTotal = setInterval(() => {
+        if (i <= totalValue) {
+          total.textContent = i;
+          i++;
+        } else if (i > totalValue) {
+            clearInterval(intervalTotal);
+            total.textContent = totalValue;
+        }
+      }, timeInterval);
+    }
 
-    animate({
-      duration: 1500,
-      timing(timeFraction) {
-        return timeFraction;
-      },
-      draw(progress) {
-        total.style.opacity = progress
-      
-      }
-    });
 
+    // setInterval(() => {
+    //   for(let i = 0; i <= totalValue; i++) {
+
+    //     total.textContent = totalValue
+    //   }
+
+    // }, 1000)
+
+    // animate({
+    //   duration: 1500,
+    //   timing(timeFraction) {
+    //     return timeFraction;
+    //   },
+    //   draw(progress) {
+    //     total.style.opacity = progress
+
+    //   }
+    // });
   };
 
   calcBlock.addEventListener("input", (e) => {
